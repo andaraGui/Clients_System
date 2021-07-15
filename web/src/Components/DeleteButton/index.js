4619915b58efbc0a60c5f5b5730823f281ff56a4
andaraGui <guiandaradev@gmail.com>
2021-07-15 13:34:55 -0300

    add confirmation ask when excluding a user

--- web/src/Components/DeleteButton/index.js
+++ web/src/Components/DeleteButton/index.js
@@ -1,21 +1,30 @@
 import * as S from './styled';
 import axios from 'axios';
-export default function DeleteButton({userName, userId, setSearchUsers}){
+export default function DeleteButton({ userName, userId, setSearchUsers }) {
 
-    function deleteHandler(){
+    function deleteUser() {
+
         axios.delete(`http://localhost:3333/users/${userId}`)
-            .then(response =>{
-                alert(`Usuário : ${userName} excluido com sucesso!`);
+            .then(response => {
+                alert(`Usuário "${userName}" excluido com sucesso!`);
                 setSearchUsers(true);
             })
-            .catch(error =>{
+            .catch(error => {
                 console.log(error);
             })
+
     }
 
-    return(
+    function deleteConfirm() {
+        const confirmDelete = window.confirm(`Tem certeza que deseja EXCLUIR o usuário "${userName}" do banco de dados?`);
+        if (confirmDelete) {
+            deleteUser()
+        }
+    }
+
+    return (
         <>
-            <button onClick={deleteHandler}>Deletar</button>
+            <button onClick={deleteConfirm}>Deletar</button>
         </>
     );
 }
