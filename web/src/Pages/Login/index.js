ef32fe4a7b414a624ab5adad55ad7d2dac0de78e
andaraGui <guiandaradev@gmail.com>
2021-07-23 16:38:10 -0300

    functional

--- web/src/Pages/Login/index.js
+++ web/src/Pages/Login/index.js
@@ -3,12 +3,39 @@ import { Redirect } from 'react-router-dom'
 import { apiUsers } from '../../services/api';
 
 
-export default function Login({setLoggedIn, loggedIn}) {
+export default function Login({ setLoggedIn, loggedIn }) {
 
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    async function buttonHandler(e) {
+    const [registerName, setRegisterName] = useState('');
+    const [registerEmail, setRegisterEmail] = useState('');
+    const [registerPassword, setRegisterPassword] = useState('');
+
+    async function registerHandler(e) {
+        e.preventDefault();
+        await apiUsers.post(`/`, { name: registerName, email: registerEmail, password: registerPassword })
+            .then(response => {
+                alert(`Usuário cadastrado com sucesso.`);
+            })
+            .catch(error => {
+                alert(`Usuário e/ou senha inválido(s)`);
+            })
+        await apiUsers.post(`/auth`, { email: registerEmail, password: registerPassword })
+            .then(response => {
+                alert(`Logged in`);
+                localStorage.setItem("token", response.data.token);
+            })
+            .catch(error => {
+                alert(`Usuário e/ou senha inválido(s)`)
+            })
+        setLoggedIn(true);
+
+    }
+
+
+    async function loginHandler(e) {
         e.preventDefault();
         await apiUsers.post(`/auth`, { email: email, password: password })
             .then(response => {
@@ -18,16 +45,26 @@ export default function Login({setLoggedIn, loggedIn}) {
             .catch(error => {
                 alert(`Usuário e/ou senha inválido(s)`)
             })
-            setLoggedIn(true);
+        setLoggedIn(true);
     }
 
     return (
         <>
             {loggedIn && <Redirect to="/" />}
+            <h3>Login</h3>
             <form>
                 Login: <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 Senha: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button onClick={buttonHandler}>ENTRAR</button>
+                <button onClick={loginHandler}>ENTRAR</button>
+            </form>
+
+
+            <h3>Cadastro de usuário:</h3>
+            <form>
+                Nome: <input type="text" value={registerName} onChange={(e) => setRegisterName(e.target.value)} />
+                Login: <input type="email" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value)} />
+                Senha: <input type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} />
+                <button onClick={registerHandler}>cadastrar</button>
             </form>
         </>
     )
